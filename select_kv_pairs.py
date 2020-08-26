694e593d66c8ea7052355bb67e6d59092c6f60b7
Paul H Bartley <paul.h.bartley@gmail.com>
2020-08-25 20:48:43 -0400

    select_kv_pairs.py

--- select_kv_pairs.py
+++ select_kv_pairs.py
@@ -4,6 +4,6 @@ kv_pairs = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':1
 
 preferred_keys = ['a', 'c', 'e', 'g', 'i']
 
-select_kv_pairs = dict((key,kv_pairs[key]) for key in preferred_keys)
+select_kv_pairs = {(key,kv_pairs[key]) for key in preferred_keys}
 
 print(select_kv_pairs)
